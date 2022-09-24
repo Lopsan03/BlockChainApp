ff8f786335ce91e0cadf9cae8b1da07ef61023dd
i343SPARK <pablosalas_@outlook.com>
2022-09-24 17:10:31 -0500

    Suma Restar Ganar

---
+++ src/components/Index/IndexAboutUs.js
@@ -0,0 +1,20 @@
+export const IndexAboutUs = () =>{
+    return(
+        <section className={"about-us-section"}>
+            <article className={"about-us-container"}>
+                <div className={"about-us-container--h2"}>
+                    <h2>Our Company</h2>
+                </div>
+                <div className={"about-us-container--p"}>
+                    <p>
+                        We are part of a venture aimed at those<br/>
+                        who want to completely control their<br/>
+                        financial world in a completely<br/>
+                        independent way.
+                    </p>
+                </div>
+                <div className={"about-us-container--img"}></div>
+            </article>
+        </section>
+    )
+}
--- src/components/Index/IndexScreen.js
+++ src/components/Index/IndexScreen.js
@@ -1,11 +1,13 @@
 import {NavBar} from "./NavBar";
 import {IndexHero} from "./IndexHero";
+import {IndexAboutUs} from "./IndexAboutUs";
 
 export const IndexScreen = () =>{
     return(
         <>
             <NavBar/>
             <IndexHero/>
+            <IndexAboutUs/>
         </>
     )
 }
