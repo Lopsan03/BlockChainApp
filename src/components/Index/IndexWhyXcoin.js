f74554ba896aaa9d45502ac154cbd6c4fff1347d
i343SPARK <pablosalas_@outlook.com>
2022-09-26 09:38:02 -0500

    index endMotherF0ckrSh1s

--- src/components/Index/IndexWhyXcoin.js
+++ src/components/Index/IndexWhyXcoin.js
@@ -9,19 +9,19 @@ export const IndexWhyXcoin = () =>{
                     <section className={"why-xcoin--information-section"}>
                         <div className={"why-xcoin--information--divisor"}>
                             <h3>Balance</h3>
-                            <span className={"money-icon"}></span>
+                            <div className={"bounced-icon"}></div>
                         </div>
                         <div className={"why-xcoin--information--divisor"}>
                             <h3>Savings</h3>
-                            <span className={"pig-icon"}></span>
+                            <div className={"pig-icon"}></div>
                         </div>
                         <div className={"why-xcoin--information--divisor"}>
                             <h3>Pay</h3>
-                            <span className={"pay-icon"}></span>
+                            <div className={"pay-icon"}></div>
                         </div>
                         <div className={"why-xcoin--information--divisor"}>
                             <h3>Transactions</h3>
-                            <span className={"change-icon"}></span>
+                            <div className={"change-icon"}></div>
                         </div>
                     </section>
                     <section className={"left-image-xcoin"}></section>
