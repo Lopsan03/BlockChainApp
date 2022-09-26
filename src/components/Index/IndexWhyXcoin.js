2528d7175206639a1bfcf57db32c745cadbe1cf0
i343SPARK <pablosalas_@outlook.com>
2022-09-26 01:49:31 -0500

    little adds

--- src/components/Index/IndexWhyXcoin.js
+++ src/components/Index/IndexWhyXcoin.js
@@ -5,21 +5,21 @@ export const IndexWhyXcoin = () =>{
                 <div className={"why-xcoin-container--title"}>
                     <h2>Our Services</h2>
                 </div>
-                <div>
-                    <section className={"why-xcoin-container-information"}>
-                        <div className={"why-xcoin-container-information--divisor"}>
+                <div className={"why-xcoin-container-information"}>
+                    <section className={"why-xcoin--information-section"}>
+                        <div className={"why-xcoin--information--divisor"}>
                             <h3>Balance</h3>
                             <span className={"money-icon"}></span>
                         </div>
-                        <div className={"why-xcoin-container-information--divisor"}>
+                        <div className={"why-xcoin--information--divisor"}>
                             <h3>Savings</h3>
                             <span className={"pig-icon"}></span>
                         </div>
-                        <div className={"why-xcoin-container-information--divisor"}>
+                        <div className={"why-xcoin--information--divisor"}>
                             <h3>Pay</h3>
                             <span className={"pay-icon"}></span>
                         </div>
-                        <div className={"why-xcoin-container-information--divisor"}>
+                        <div className={"why-xcoin--information--divisor"}>
                             <h3>Transactions</h3>
                             <span className={"change-icon"}></span>
                         </div>
