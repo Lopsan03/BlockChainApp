ba66472b7b2f5888778827b7e62690fff3d60dbe
i343SPARK <pablosalas_@outlook.com>
2022-10-01 14:43:49 -0500

    adding-diving

--- src/components/DashBoard/DsBdNavBar.js
+++ src/components/DashBoard/DsBdNavBar.js
@@ -4,16 +4,14 @@ export const DsBdNavBar = () =>{
     return(
         <section className={"dsbd-navbar-section"}>
             <article className={"dsbd-navbar-container"}>
-                <div>
-                    <Link to={"/DashBoard"}>XCOIN</Link>
+                <div className={"dsbd-main-logo--div"}>
+                    <Link className={"dsbd-main-logo-link"} to={"/DashBoard"}>XCOIN</Link>
                 </div>
-                <div>
-                    <li>
-                        <Link to={"/DashBoard"} className={"dsbr-link"}>Dash Board</Link>
-                        <Link to={"/DashBoard"} className={"dsbr-link"}>Dash Board</Link>
-                        <Link to={"/DashBoard"} className={"dsbr-link"}>Dash Board</Link>
-                        <Link to={"/DashBoard"} className={"dsbr-link"}>Dash Board</Link>
-                    </li>
+                <div className={"dsbd-data-options--div"}>
+                    <Link to={"/DashBoard"} className={"dsbr-link"}>Home</Link>
+                    <Link to={"/DashBoard"} className={"dsbr-link"}>Transactions</Link>
+                    <Link to={"/DashBoard"} className={"dsbr-link"}>Pay</Link>
+                    <Link to={"/DashBoard"} className={"dsbr-link"}>Savings</Link>
                 </div>
             </article>
         </section>
